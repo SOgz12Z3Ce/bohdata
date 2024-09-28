a244f1234a64d1bf03a02b7a0cd9cd495e5a7d34
SOgz12Z3Ce <ursername158481@gmail.com>
2024-09-28 20:42:45 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bohdata',
+    version='0.1',
+    packages=find_packages(),
+    description='处理《司辰之书》游戏数据的 python 工具。',
+    author='SOgz12Z3Ce',
+    author_email='ursername158481@gmail.com',
+    url='https://github.com/SOgz12Z3Ce/bohdata',
+)

9f825b79d264851cd166a074809987d30e03f2a7
SOgz12Z3Ce <ursername158481@gmail.com>
2024-09-28 21:09:06 +0800

    完善 JSON 编码报错

--- bohdata/file.py
+++ bohdata/file.py
@@ -1,19 +1,24 @@
 import json
 
 class UnexpectedEncoding(Exception):
-	"""意料之外的编码格式，在尝试读取编码不为 UTF-8、UTF-8 with BOM、UTF-16LE 之一的文件时抛出。"""
+	"""Unexpected encoding format. Raised when trying to read a file that is not encoded in UTF-8, UTF-8 with BOM, or UTF-16LE."""
 	def __init__(self, message):
 		super().__init__(message)
 
 def read(path):
-	encodings = ['utf-8', 'utf-8-sig', 'utf-16-le'] # A·K 的常用编码
+	encodings = ['utf-8', 'utf-8-sig', 'utf-16-le']  # Common encodings for A·K
 	for encoding in encodings:
 		try:
 			with open(path, 'r', encoding=encoding) as file:
 				content = file.read()
 		except UnicodeDecodeError:
 			continue
+		except json.decoder.JSONDecodeError as error:
+			print(f'Error parsing "{path}":')
+			print(error)
+			print('Tip: Check A·K\'s JSON file, which may contain errors.')
+
 		if content.startswith('\ufeff'):
 			content = content[1:]
 		return json.loads(content)
-	raise UnexpectedEncoding(f'"{path}"未使用 UTF-8、UTF-8 with BOM 或 UTF-16LE 编码。')
+	raise UnexpectedEncoding(f'"{path}" is not encoded in UTF-8, UTF-8 with BOM, or UTF-16LE.')

72e25c686f8e03aad231c710db4b3e1ea0cb0540
SOgz12Z3Ce <ursername158481@gmail.com>
2024-09-28 21:28:47 +0800

    改进结构，添加注释

--- bohdata/file.py
+++ bohdata/file.py
@@ -7,18 +7,31 @@ class UnexpectedEncoding(Exception):
 
 def read(path):
 	encodings = ['utf-8', 'utf-8-sig', 'utf-16-le']  # Common encodings for A·K
+	# 尝试打开文件
+	content = ''
 	for encoding in encodings:
 		try:
 			with open(path, 'r', encoding=encoding) as file:
 				content = file.read()
 		except UnicodeDecodeError:
 			continue
-		except json.decoder.JSONDecodeError as error:
-			print(f'Error parsing "{path}":')
-			print(error)
-			print('Tip: Check A·K\'s JSON file, which may contain errors.')
+	
+	# 打开文件失败
+	if content == '':
+		raise UnexpectedEncoding(f'"{path}" is not encoded in UTF-8, UTF-8 with BOM, or UTF-16LE.')
 
-		if content.startswith('\ufeff'):
-			content = content[1:]
-		return json.loads(content)
-	raise UnexpectedEncoding(f'"{path}" is not encoded in UTF-8, UTF-8 with BOM, or UTF-16LE.')
+	# 删除bom字符
+	if content.startswith('\ufeff'):
+		content = content[1:]
+	
+	# 读取文件为json
+	try:
+		jsonData = json.loads(content)
+	except json.decoder.JSONDecodeError as error:
+		raise json.decoder.JSONDecodeError(
+			f'Error parsing "{path}": {error.msg}\nTip: Check A·K\'s JSON file, which may contain errors.', 
+			error.doc, 
+			error.pos
+		) from error
+
+	return jsonData
